f2e28551ccdd73b00250c82b44e1ccc09ba9510f
mohammad reza khosravi <mrez9090@gmail.com>
2022-07-09 22:29:47 +0430

    some fixes in kgs

--- internal/adapters/kgs/main.go
+++ internal/adapters/kgs/main.go
@@ -41,12 +41,12 @@ func (kgs *kgs) updateCounter() {
 }
 
 func (kgs *kgs) fillSeed() {
-	for _, range_ := range [][2]byte{
+	for _, r := range [][2]byte{
 		{'0', '9'}, // numbers
 		{'a', 'z'}, // lowercase
 		{'A', 'Z'}, // uppercase
 	} {
-		from, to := range_[0], range_[1]
+		from, to := r[0], r[1]
 
 		for i := from; i <= to; i++ {
 			kgs.seed = append(kgs.seed, i)
@@ -54,16 +54,23 @@ func (kgs *kgs) fillSeed() {
 	}
 }
 
-func (kgs *kgs) GetKey() string {
+func (kgs *kgs) convertToBase62(c int64) string {
 	key := make([]byte, 0, 10)
-	kgs.mu.Lock()
-
 	for c := float64(kgs.counter); c != 0; {
 		key = append(key, kgs.seed[int(c)%62])
 		c = math.Floor(c / 62)
 	}
+	return string(key)
+}
+
+func (kgs *kgs) GetKey() string {
+	kgs.mu.Lock()
+
+	key := kgs.convertToBase62(kgs.counter)
 
 	kgs.updateCounter()
+	// not using defer for performance reasons
+	// reference: https://medium.com/i0exception/runtime-overhead-of-using-defer-in-go-7140d5c40e32
 	kgs.mu.Unlock()
 	return string(key)
 }
