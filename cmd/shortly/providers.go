a5d42f3d361d4ea76f62ae219e6215a1ffd6541d
mohammad reza khosravi <mrez9090@gmail.com>
2022-07-09 16:26:02 +0430

    move cmd/* to cmd/shortly

---
+++ cmd/shortly/providers.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	cassandra_repo "github.com/mreza0100/shortly/internal/adapters/cassandra"
+	cassandra_connection "github.com/mreza0100/shortly/internal/pkg/connection/cassandra"
+
+	"github.com/mreza0100/shortly/internal/adapters/kgs"
+	"github.com/mreza0100/shortly/internal/ports"
+)
+
+func getCassandraRepo(cfg *cassandraConnectionConfigs) (ports.CassandraReadPort, ports.CassandraWritePort) {
+	session, err := cassandra_connection.CreateConnection(&cassandra_connection.ConnectionConfigs{
+		Host:     cfg.Host,
+		Port:     cfg.Port,
+		Keyspace: cfg.Keyspace,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cassandraRead, cassandraWrite, err := cassandra_repo.NewCassandraRepository(session)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return cassandraRead, cassandraWrite
+}
+
+func newKGS(cassandraRead ports.CassandraReadPort, cassandraWrite ports.CassandraWritePort) (ports.KGS, error) {
+	counter, err := cassandraRead.GetCounter(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	kgs := kgs.New(kgs.InitKGSOptions{
+		SaveCounter: func(newCounter int64) {
+			if err := cassandraWrite.UpdateCounter(context.Background(), newCounter); err != nil {
+				log.Fatal("Failed to save counter: ", err)
+			}
+		},
+		LastSavedCounter: counter,
+	})
+
+	return kgs, nil
+}
